94d89f99a651dffd6e4682e4b1a18c718c692988
Felipe Guilherme <108408157+felipelopxs@users.noreply.github.com>
2023-03-07 22:20:52 -0300

    Add files via upload

---
+++ NotasAlunos/src/exercicio3/Main.java
@@ -0,0 +1,9 @@
+package exercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno Aluno1 = new Aluno("Pedro","3117",27.11,35.70,"Sistemas de Informação",3);
+        Aluno1.avaliarAluno();
+        Aluno1.avaliarRecuperacao(80);
+    }
+}
