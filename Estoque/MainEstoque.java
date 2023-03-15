2747803c47f4a8933d8d7ede9648e2d0056e31c3
Felipe Guilherme <108408157+felipelopxs@users.noreply.github.com>
2023-03-14 22:07:40 -0300

    Update MainEstoque.java

--- Estoque/MainEstoque.java
+++ Estoque/MainEstoque.java
@@ -2,11 +2,17 @@ package br.com.estoque;
 
 public class MainEstoque {
     public static void main(String[] args) {
-        Estoque e =new Estoque();
+        Estoque e = new Estoque();
         e.setNome("Caneta");
         e.setQtdAtual(18);
         e.setQtdMinima(5);
         System.out.println(e.mostra());
+
+        Estoque e1 = new Estoque(1, "borracha",12,5);
+        e1.darBaixa(10);
+        System.out.println("Estoque atual: " + e1.getQtdAtual());
+        e1.darBaixa(10);
+        System.out.println(e1.precisaRepor());
 
     }
 }
